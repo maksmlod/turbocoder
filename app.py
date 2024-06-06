55c02104b18b0f5a2a6e2eb2d4d9d10e28d7f7ca
maks <01178297@pw.edu.pl>
2024-06-06 15:16:11 +0200

    showing channel vectors

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def index():
         awgn_level = int(request.form['awgn_level'])
         generate_plot_flag = 'generate_plot' in request.form
         interleaver = random.sample(range(len(input_vector)), len(input_vector))
-        encoded_output, decoded_output = process_data(input_vector, interleaver, awgn_level)
+        encoded_output, decoded_output, channel_vector_before, channel_vector_after = process_data2(input_vector, interleaver, awgn_level)
         error_rate = calculate_error_rate(input_vector, decoded_output)
 
         if generate_plot_flag:
@@ -28,13 +28,15 @@ def index():
 
         return render_template('index.html', input_vector=input_vector, interleaver=interleaver,
                                awgn_level=awgn_level, encoded_output=encoded_output, decoded_output=decoded_output,
-                               error_rate=error_rate, generate_plot_flag=generate_plot_flag)
+                               error_rate=error_rate, generate_plot_flag=generate_plot_flag,
+                               channel_vector_before=channel_vector_before, channel_vector_after=channel_vector_after)
     return render_template('index.html', awgn_level=awgn_level)
 
 
 def process_data(input_vector, interleaver, awgn_level):
     encoder = TurboEncoder(interleaver)
     decoder = TurboDecoder(interleaver)
+    if awgn_level == 0: awgn_level = -1000
     channel = AWGN(awgn_level)
 
     encoded_vector = encoder.execute(input_vector)
@@ -51,9 +53,37 @@ def process_data(input_vector, interleaver, awgn_level):
     return encoded_vector, decoded_vector
 
 
+def process_data2(input_vector, interleaver, awgn_level):
+    encoder = TurboEncoder(interleaver)
+    decoder = TurboDecoder(interleaver)
+    channel1 = AWGN(-1000)
+    if awgn_level == 0: awgn_level = -1000
+    channel2 = AWGN(awgn_level)
+
+    encoded_vector = encoder.execute(input_vector)
+    encoded_vector2 = encoded_vector
+
+    channel_vector_before = list(map(float, encoded_vector))
+    channel_vector_before = channel1.convert_to_symbols(channel_vector_before)
+    channel_vector_before = channel1.execute(channel_vector_before)
+
+
+    channel_vector_after = list(map(float, encoded_vector2))
+    channel_vector_after = channel2.convert_to_symbols(channel_vector_after)
+    channel_vector_after = channel2.execute(channel_vector_after)
+
+    decoded_vector = decoder.execute(channel_vector_after)
+    decoded_vector = [int(b > 0.0) for b in decoded_vector]
+
+    decoded_vector = decoded_vector[:-2]
+
+    return encoded_vector, decoded_vector, channel_vector_before, channel_vector_after
+
+
 def calculate_error_rate(input_vector, decoded_vector):
-    error_count = sum(1 for x, y in zip(input_vector, decoded_vector[:-2]) if x != y)
+    error_count = sum(1 for x, y in zip(input_vector, decoded_vector) if x != y)
     total_bits = len(input_vector)
+    print(error_count)
     error_rate = (error_count / total_bits) * 100
     return error_rate
 
@@ -73,9 +103,9 @@ def generate_plot(input_vector):
 
     plt.figure(figsize=(6, 4))
     plt.plot(awgn_values, error_rates, marker='o', linestyle='-')
-    plt.title('Average Error Rate vs. AWGN Level')
-    plt.xlabel('AWGN')
-    plt.ylabel('Average Error Rate (%)')
+    plt.title('Average Error Rate(AWGN)')
+    plt.xlabel('AWGN [dB]')
+    plt.ylabel('Average Error Rate [%]')
     plt.grid(True)
     plt.savefig('static/error_rate_plot.png')
     plt.close()
